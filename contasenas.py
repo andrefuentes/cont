2a694ab48323deafb8d327e2a70beb13cc9c67ba
andrefuentes <andrefuentes166@gmail.com>
2017-02-17 08:54:46 -0600

    3.0

---
+++ contasenas.py
@@ -0,0 +1,7 @@
+import random
+def cantidad_con(cont):
+	f=opend("cont.txt")
+	f.write("contasenas\n")
+def pasword(letras,cantidad,longitud):
+	for i in range(cantidad):
+		for j in range(longitud):
